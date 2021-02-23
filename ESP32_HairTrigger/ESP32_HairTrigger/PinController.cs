a36f1e947b8f36e2fbb5e8dbd66bd3b278c942ab
Austin Albert <aalbert@tipconsulting.io>
2021-02-22 20:14:35 -0700

    Add ESP32 project with MPU/ArdNet

---
+++ ESP32_HairTrigger/ESP32_HairTrigger/PinController.cs
@@ -0,0 +1,39 @@
+﻿using System.Device.Gpio;
+using System.Threading;
+
+namespace ESP32_HairTrigger
+{
+    public static class PinController
+    {
+        public static GpioController Default { get; } = new GpioController();
+
+        public class LedBlinker
+        {
+            private readonly GpioPin _ledPin;
+            private readonly Timer _onboardBlinkTimer;
+            public LedBlinker(int PinNumber)
+            {
+                _ledPin = Default.OpenPin(PinNumber);
+                _ledPin.SetPinMode(PinMode.Output);
+                _onboardBlinkTimer = new Timer(ShutdownTimerCallback, _ledPin, Timeout.Infinite, Timeout.Infinite);
+            }
+
+            private void ShutdownTimerCallback(object e)
+            {
+                ((GpioPin)e).Write(PinValue.Low);
+            }
+
+            public void On() => _ledPin.Write(PinValue.High);
+
+            public void Off() => _ledPin.Write(PinValue.Low);
+
+            public void Blink(int millis)
+            {
+                _ledPin.Write(PinValue.High);
+                _ = _onboardBlinkTimer.Change(millis, Timeout.Infinite);
+            }
+
+        }
+
+    }
+}
--- ESP32_HairTrigger/ESP32_HairTrigger/Program.cs
+++ ESP32_HairTrigger/ESP32_HairTrigger/Program.cs
@@ -3,30 +3,66 @@ using System.Diagnostics;
 using System.Threading;
 using System.Device.I2c;
 using nanoFramework.Hardware.Esp32;
+using ArdNet.Nano.Client;
+using ArdNet.Nano;
+using System.Collections;
 
 namespace ESP32_HairTrigger
 {
     public class Program
     {
-        static Mpu6050 _mpu;
+        private static PinController.LedBlinker _led = null;
+        private static Mpu6050 _mpu;
+        private static ArdNetClientManager _ardManager;
 
         public static void Main()
         {
             Configuration.SetPinFunction(23, DeviceFunction.I2C1_DATA);
             Configuration.SetPinFunction(22, DeviceFunction.I2C1_CLOCK);
-
             _mpu = Mpu6050.StartNew(1);
+
+
+            Debug.WriteLine("Init");
+            _led = new PinController.LedBlinker(13);
+            _led.On();
+            Thread.Sleep(100);
+            _led.Off();
+
+
+            var AppID = "Esp32.HairTrigger";
+            var serverUdpPort = 48597;
+            var nanoConfig = new ArdNetClientConfig(AppID, null, serverUdpPort);
+            nanoConfig.TCP.HeartbeatConfig.HeartbeatInterval = TimeSpan.FromMilliseconds(2000);
+            nanoConfig.TCP.HeartbeatConfig.ForceStrictHeartbeat = true;
+            nanoConfig.TCP.HeartbeatConfig.RespondToHeartbeats = false;
+
+            _ardManager = new ArdNetClientManager(SystemConfig.WiFiCredentials, nanoConfig);
+            _ardManager.StartWorkerThread();
+
 
             Debug.WriteLine("Loop");
 
             while (true)
             {
                 var data = _mpu.GetData();
-                Debug.WriteLine($"Accel: [{data.AccelX}, {data.AccelY}, {data.AccelZ}] | Gyro: [{data.GyroX}, {data.GyroY}, {data.GyroZ}]");
+                var args = new ArrayList()
+                {
+                    data.AccelX.ToString(),
+                    data.AccelY.ToString(),
+                    data.AccelZ.ToString(),
+                    data.GyroX.ToString(),
+                    data.GyroY.ToString(),
+                    data.GyroZ.ToString()
+                };
+
+                var request = TcpRequest.CreateOutbound("Device.Sensors.MPU", args);
+                _ardManager.EnqueueTask(x =>
+                {
+                    _ = x.SendCommand(request, null);
+                });
                 Thread.Sleep(50);
             }
 
         }
-
     }
 }
